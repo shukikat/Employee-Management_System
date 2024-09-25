3db95d4b53e551207f19390fd0602fd2c211780f
shukikat <kathuriashuki@gmail.com>
2024-09-25 10:57:39 -0400

    updated schemasql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,27 @@
-DROP DATABASE IF EXISTS courses_db;
-CREATE DATABASE courses_db;
+DROP DATABASE IF EXISTS employeemanagement_db;
+CREATE DATABASE employeemanagement_db;
 
-\c courses_db;
+\c employeemanagement_db;
 
-CREATE TABLE course_names (
+CREATE TABLE department (
   id SERIAL PRIMARY KEY,
-  name VARCHAR(30) NOT NULL
-)
+  name VARCHAR(30) UNIQUE NOT NULL
+
+);
+
+CREATE TABLE role (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL NOT NULL,
+  department_id INTEGER NOT NULL,
+  FOREIGN KEY (department_id) REFERENCES department(id)
+); 
+
+CREATE TABLE employee (
+  id SERIAL PRIMARY KEY,
+  first_name VARCHAR(30) UNIQUE NOT NULL,
+  last_name VARCHAR(30) UNIQUE NOT NULL,
+  role_id INTEGER NOT NULL,
+  manager_id INTEGER, 
+  FOREIGN KEY (role_id) REFERENCES role(id)
+); 
